15532ace2da531853baa344648cdc94d0e0e51b1
CarnegieSenpai <sawyer3nick@gmail.com>
2021-12-14 22:34:01 -0800

    Extremely basic electron setup. public directory is useless rn

---
+++ src/ui-pages/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import logo from "./logo512.png"
+import { Button } from "@mui/material"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button onClick={()=> {console.log("clicked button")}}>Click me</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
