8837a732a8ed69c81d4725d383008990315ca226
Nick <quantumbogo@protonmail.com>
2023-09-07 02:09:36 -0700

    Comment in index

--- src/ui-pages/index.ts
+++ src/ui-pages/index.ts
@@ -1,4 +1,7 @@
-const req = require.context('./', true, /.js$/);
+// This index dynamically picks up all files in ui-pages
+// It should not require modification
+
+const req = require.context('./', true, /.js$/); // Looking for .js files because by the time it makes it to webpack the code has compiles to js
 
 const modules = req.keys().map(req)
 
